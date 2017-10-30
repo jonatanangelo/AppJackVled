fbecd9acd8900025254f8df34857fbe0ab497e4f
Jonatán López <jonatan_angelo@hotmail.com>
2017-10-30 17:32:36 -0500

    com

--- www/js/about.js
+++ www/js/about.js
@@ -6,4 +6,19 @@ var About = function(){
 		//$("#about").transition({x:1024},0);
 		//$("#about").transition({x:0},600,'easeOutCirc',onComplete);
 	}
+	this.parar = function(){
+		document.getElementById("video").pause();	
+		document.getElementById("video").currentTime = 0;
+		$("#about .preview").fadeIn(300);
+	}
+	new Boton($("#about .preview"),function(bt){
+		bt.hide();
+		document.getElementById("video").play();
+	});
+
+	document.getElementById('video').addEventListener('ended',function(){
+		about.parar();
+		//alert(1);
+
+	},false);
 }
