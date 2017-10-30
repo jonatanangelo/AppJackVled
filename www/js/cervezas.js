c480cb1c4c4686bd4826d473b3a035454aaf114f
Jonatán López <jonatan_angelo@hotmail.com>
2017-10-30 01:50:35 -0500

    anim cervexzas 2

--- www/js/cervezas.js
+++ www/js/cervezas.js
@@ -41,21 +41,29 @@ var Cervezas = function(){
 	});
 
 	$("#cervezas .slider").on("swiperight",function(){
-		cervezas.ac--;
-		if(cervezas.ac==0) cervezas.ac=4;
+		b.eq(ac).transition({x:290,scale:0.9},300);
+		
+		var ant = ac-1;
+		if(ant==-1) ant=3;
+		//console.log("principal"+sig);
+		b.eq(ant).transition({x:0,scale:1},300);
 
-		var nuevo = cervezas.ac-1;
-		if(nuevo==0) nuevo = 4;
+		var sig = ac+1;
+		if(sig==4) sig=0;
+		//console.log("se va"+ant);
+		b.eq(sig).transition({opacity:0},300);
 
+		var nuevo = ac-2;
+		if(nuevo==-1) nuevo=3;
+		if(nuevo==-2) nuevo=2;
+		//console.log("derecha"+nuevo);
+		b.eq(nuevo).transition({x:-290,scale:0.9},0);
+		b.eq(nuevo).transition({opacity:1},600);
 
+		
+		ac--;
 
-		$("#cervezas .slider .botella.der").transition({opacity:0},300);
-		$("#cervezas .slider .botella.cen").removeClass("cen").addClass("der");
-		$("#cervezas .slider .botella.izq").removeClass("izq").addClass("cen");
-
-		var divnuevo = $('<div class="botella izq" style="opacity:0;background-image: url(img/cervezas/'+cervezas.lista[nuevo-1]+'/cerveza.png)"></div>');
-		$("#cervezas .slider").append(divnuevo);
-		divnuevo.animate({opacity:1},200);
+		if(ac==-1) ac=3;
 
 	});
 
